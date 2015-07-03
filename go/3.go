4466e504b93230b2d7e2041801d9cac97155268b
chetaldrich <chetaldrich@gmail.com>
2015-07-02 22:42:42 -0700

    Added solution 4 in go

---
+++ go/3.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "unicode/utf8"
+)
+
+func isPalindrome(drome string) bool {
+
+    dromelen := utf8.RuneCountInString(drome)
+
+    // base case
+    if dromelen == 1 || dromelen == 0 {
+        return true
+    }
+
+    // induction
+    if drome[0] == drome[dromelen - 1] {
+        thing := drome[1:dromelen - 1]
+        return isPalindrome(thing)
+    }
+
+    return false
+}
+
+func main() {
+
+    largest := 0
+
+    for x := 100; x < 1000; x++ {
+        for y := 100; y < 1000; y++ {
+            z := x * y
+            zstr := strconv.Itoa(z)
+            if (isPalindrome(zstr) && largest <= z) {
+                largest = z
+            }
+        }
+    }
+
+    fmt.Printf("%d\n", largest)
+}

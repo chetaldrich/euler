578f9ca9b2a24dd709a62d512aa14d0dcb1b0d9d
chetaldrich <chetaldrich@gmail.com>
2015-08-03 22:08:32 -0700

    Euler 3 in Scala

---
+++ scala/3/3.scala
@@ -0,0 +1,14 @@
+/**
+ * A solution to Euler Problem 3
+ */
+object Problem_3 {
+    def main(args: Array[String]) = {
+        var limit = 600851475143L
+        lazy val naturals: Stream[Int] = Stream.cons(1, naturals.map(_ + 1))
+        val primeFactors =
+            naturals.drop(1).dropWhile(
+                n => {while(limit % n == 0) {limit /= n}; limit > 1}
+            );
+        println(primeFactors.head)
+    }
+}

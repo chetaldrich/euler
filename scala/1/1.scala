9400713ddc3b27c7e91efb7e3f55ad6b7961e90a
chetaldrich <chetaldrich@gmail.com>
2015-08-08 22:56:30 -0700

    Solution to Euler 4 in Scala.
    Also, found out how neat views are in terms of memory and allowing
    function composition and stuff to happen without actually creating
    collections in between, which seems to be faster.

--- scala/1/1.scala
+++ scala/1/1.scala
@@ -4,7 +4,11 @@
  */
 object Problem_1 {
     def main(args: Array[String]) {
-        val solution = (1 until 1000).filter(x => x % 3 == 0 || x % 5 == 0).sum
+        val solution = (1 until 1000)
+          .view
+          .filter(x => x % 3 == 0 || x % 5 == 0)
+          .sum
+        
         println(solution)
     }
 }

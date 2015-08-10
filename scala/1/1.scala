dc8790d1e85ad9b74f86670a1386447b044618ef
chetaldrich <chetaldrich@gmail.com>
2015-08-10 10:22:19 -0700

    Styling convention changes
    Realized that Scala prefers 2 space indentation

--- scala/1/1.scala
+++ scala/1/1.scala
@@ -3,12 +3,12 @@
  * author: Chet Aldrich
  */
 object Problem_1 {
-    def main(args: Array[String]) {
-        val solution = (1 until 1000)
-          .view
-          .filter(x => x % 3 == 0 || x % 5 == 0)
-          .sum
-        
-        println(solution)
-    }
+  def main(args: Array[String]) {
+    val solution = (1 until 1000)
+      .view
+      .filter(x => x % 3 == 0 || x % 5 == 0)
+      .sum
+
+    println(solution)
+  }
 }

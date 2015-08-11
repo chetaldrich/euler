8947deb1feda9085075a4048bd87c80544654e12
chetaldrich <chetaldrich@gmail.com>
2015-08-10 22:24:32 -0700

    Solution 5 in Scala
    Got a chance to use a case method and folding and currying (built-in,
    but still curried), which was pretty exciting.

---
+++ scala/5/5.scala
@@ -0,0 +1,18 @@
+/**
+ * A solution to Euler problem 5.
+ */
+object Problem_5 {
+    def gcd(a: Long, b: Long): Long = (a, b) match {
+      case (a, b) if b == a => a
+      case (a, b) if b > a  => gcd(b, a)
+      case (a, 0)           => a
+      case _                => gcd(b, a % b)
+    }
+
+    def lcm(a: Long, b: Long): Long = ((a * b) / gcd(a, b))
+
+    def main(args: Array[String]) {
+      val solution = (3 to 20).foldLeft(lcm(1, 2))((b, a) => lcm(a, b))
+      println(solution)
+    }
+}

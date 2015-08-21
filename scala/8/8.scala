4393f0e8c647694eb953a8a534bd4c8ee751cf78
chetaldrich <chetaldrich@gmail.com>
2015-08-20 21:02:20 -0700

    Added solution 8 in scala

---
+++ scala/8/8.scala
@@ -0,0 +1,20 @@
+/**
+ * Solution for Euler Problem 8
+ */
+import scala.io._
+
+object Problem_8 {
+  def main(args: Array[String]): Unit = {
+    val number = Source.fromFile("number.txt")
+    val numberString = number
+      .getLines
+      .foldLeft("")((b, a) => b + a)
+    val solution = numberString
+      .map(_.asDigit)
+      .map(_.toLong)
+      .sliding(13)
+      .map(_.product)
+      .max
+    println(solution)
+  }
+}
